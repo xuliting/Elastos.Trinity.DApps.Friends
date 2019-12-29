ca0cd578dcfb245311933582332001e11bacd11a
Chad Racelis <chadracelis@yahoo.com>
2019-12-29 16:55:28 -0500

    Design to add-friend page, design polished

--- src/app/pages/add-friend/add-friend.module.ts
+++ src/app/pages/add-friend/add-friend.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { AddFriendPage } from './add-friend.page';
+import { SharedModule } from 'src/app/modules/shared.module';
 
 const routes: Routes = [
   {
@@ -17,6 +18,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
+    SharedModule,
     FormsModule,
     IonicModule,
     RouterModule.forChild(routes)
