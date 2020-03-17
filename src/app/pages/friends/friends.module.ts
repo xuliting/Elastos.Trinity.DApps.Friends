a01f4b592a111fb7a02c6489b637bae80db449a2
Chad Racelis <chadracelis@yahoo.com>
2020-03-16 22:07:19 -0400

    Rm header, add add-friend btn to friends-list container

--- src/app/pages/friends/friends.module.ts
+++ src/app/pages/friends/friends.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { FriendsPage } from './friends.page';
+import { SharedModule } from 'src/app/modules/shared.module';
 
 const routes: Routes = [
   {
@@ -19,6 +20,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    SharedModule,
     RouterModule.forChild(routes)
   ],
   declarations: [FriendsPage]
